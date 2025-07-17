34e3b077d8bba7d480d37c36ef77bad642b02c8f
Oktay Sırmaçekici <79636703+Selulox@users.noreply.github.com>
2025-07-17 16:57:23 +0300

    second

---
+++ Scripts/bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var travelled_distance = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	const speed = 450
+	const range = 750
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * speed * delta 
+	travelled_distance += speed * delta 
+	if travelled_distance > range:
+		queue_free() 
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
+	if body.has_method('take_damage'):
+		body.take_damage()
+ 
--- Scripts/player_ship.gd
+++ Scripts/player_ship.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+func _physics_process(_delta):
+	pass
+
+func shoot():
+	const Bullet = preload("res://Scenes/bullet.tscn")
+	var new_bullet = Bullet.instantiate()
+	new_bullet.global_position = %Shoot_Point.global_position
+	new_bullet.global_rotation = %Shoot_Point.global_rotation
+	%Shoot_Point.add_child(new_bullet)
+
+
+func _on_shoot_timer_timeout() -> void:
+	shoot()
